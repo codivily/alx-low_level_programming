335d51b1024c343b1643e7b069623f0fc5b54f49
codivily <codivily@gmail.com>
2022-03-17 02:57:02 -0700

    3-print_remain_days.c fix 4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,32 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	int month_ = 1;
+	int day_ = 0;
+	int is_leap_year =
+		((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+	int is_valid_date =
+		((month % 2 == 1)
+		 || (day <= (month == 2 ? is_leap_year ? 29 : 28 : 30)))
+
+	if (is_valid_date)
 	{
-		if (month >= 2)
+		while (month_ < month)
 		{
-			day++;
+			day_ += 30 + (month_ % 2);
+			month_++;
 		}
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		if (month_ >= 2)
+			day_ -= (is_leap_year ? 1 : 2);
+
+		day_ += day;
+		printf("Day of the year: %d\n", day_);
+		printf("Remaining days: %d\n", 366 - day_);
 	}
+
 	else
 	{
-		if (month == 2)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 	}
 }
