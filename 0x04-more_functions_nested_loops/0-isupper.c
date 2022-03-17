d3ad9b3273ca72c7f2af8b891e31523b67e12afc
codivily <codivily@gmail.com>
2022-03-17 15:33:02 -0700

    0-isupper.c added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is upper case
+ * @c: character to be checked
+ *
+ * Return: 1 if uppercase 0 otherwhise
+ */
+int _isupper(int c)
+{
+	return ('A' <= c && c <= 'Z' ? 1 : 0);
+}
