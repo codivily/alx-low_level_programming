1f4fe0b6a021ed6f593690d1accf64dc688ff408
codivily <codivily@gmail.com>
2022-03-17 16:51:15 -0700

    5-more_number.c fixed fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-void print_number(int);
 
 /**
  *more_numbers - prints 10 times the numbers, from 0 to 14
@@ -16,46 +15,11 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			print_number(n);
+			if (n > 9)
+				_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
 		}
 
 		_putchar('\n');
-	}
-}
-
-/**
- *print_number - Print a given an integer number to the stdout
- *@n: An integer to be displayed
- *
- *Return: Nothing
- */
-void print_number(int n)
-{
-	int pwr_of_10 = 1;
-	int first_digit = 0;
-
-	if (n < 0)
-	{
-		n *= (-1);
-		_putchar('-');
-	}
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		pwr_of_10 = 1;
-		while (n / pwr_of_10)
-			pwr_of_10 *= 10;
-
-		while (pwr_of_10 != 1)
-		{
-			pwr_of_10 /= 10;
-			first_digit = n / pwr_of_10;
-			_putchar('0' + first_digit);
-			n -= (first_digit * pwr_of_10);
-		}
 	}
 }
