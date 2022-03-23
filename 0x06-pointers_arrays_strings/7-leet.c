500d10b31a7d5d7ab6affbb97ea1d1eda235a27e
codivily <codivily@gmail.com>
2022-03-23 11:11:37 -0700

    7-leet added

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * leet - encodes a string into 1337
  * @s: the string
@@ -8,28 +10,32 @@ char *leet(char *s)
 {
 
 	char *p = s;
-	char nums[60] = {'\0'};
+	char nums[58];
 	int i = 0;
 
+	for (i = 0; i < 58; i++)
+		nums[i] = '\0';
+
 	nums[0] = '4';
-	nums[33] = '4';
+	nums[32] = '4';
 	nums[4] = '3';
-	nums[37] = '3';
+	nums[36] = '3';
 	nums[14] = '0';
-	nums[47] = '0';
+	nums[46] = '0';
 	nums[19] = '7';
-	nums[52] = '7';
+	nums[53] = '7';
 	nums[11] = '1';
-	nums[44] = '1';
+	nums[43] = '1';
 
 	while (*p != '\0')
 	{
-		i = 'A' - *p;
+		i = (*p) - 'A';
 
-		if ('A' <= *p && *p <= 'z' && nums[i] != '\0')
+		if (i >= 0 && i < 60 && nums[i] != '\0')
 			*p = nums[i];
 		p++;
 	}
+	printf("\n");
 
 	return (s);
 }
