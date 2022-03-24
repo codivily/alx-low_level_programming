2fd23aebcc1eddf7891d5bee311d4439b6342b80
codivily <codivily@gmail.com>
2022-03-24 15:59:15 -0700

    2-strncpy.c fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 
 	char *ret = dest;
 
-	while (*src != '\0' && n > 0)
+	while (n > 0 && *src != '\0')
 	{
 		*dest = *src;
-		dest++;
 		src++;
+		dest++;
 		n--;
 	}
 
-	*dest = '\0';
+	if (*src == '\0')
+		*dest = '\0';
 
 	return (ret);
 }
