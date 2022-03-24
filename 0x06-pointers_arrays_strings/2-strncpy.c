875ab8cc4bf5c33b005b227ff1ad22a10dc541be
codivily <codivily@gmail.com>
2022-03-24 16:44:16 -0700

    2-strncpy.c more elegant code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,17 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	char *ret = dest;
 
-	while (n > 0 && *src != '\0')
-	{
+	while (n-- > 0 && *src != '\0')
 		*(dest++) = *(src++);
-		n--;
-	}
 
-	while (n > 0)
-	{
+	while (n-- > 0)
 		*(dest++) = '\0';
-		n--;
-	}
 
 	return (ret);
 }
