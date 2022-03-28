4b52a1ce5f24e3600df4230e8a17985bb00c9221
codivily <codivily@gmail.com>
2022-03-28 16:36:30 -0700

    0-memset.c added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area pointed
+ * @b: constant b
+ * @n: first n bytes count
+ *
+ * Return: A pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n-- > 0)
+		*(s + n) = b;
+}
