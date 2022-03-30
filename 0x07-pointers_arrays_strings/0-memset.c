f57021b0bac220346a28e9cdefd18b1b7338acc5
codivily <codivily@gmail.com>
2022-03-29 18:13:08 -0700

    0-memset.c updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,13 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *p = s;
+	unsigned int i = 0;
 
-	while (n > 0)
+	while (i < n)
 	{
 		*p = b;
 		p++;
-		n--;
+		i++;
 	}
 
 	return (s);
