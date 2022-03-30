310d751648fc1260e245437f2ee09aeeaf09dcf2
codivily <codivily@gmail.com>
2022-03-29 18:45:29 -0700

    8-print_diagsusm.c updated

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,15 +10,14 @@
 void print_diagsums(int *a, int size)
 {
 
-	int **matrix = a;
 	int x = 0;
 	int major_diag_sum = 0;
 	int minor_diag_sum = 0;
 
 	for (x = 0; x < size; x++)
 	{
-		major_diag_sum += matrix[x][x];
-		minor_diag_sum += matrix[x][size - x - 1];
+		major_diag_sum += a[(size * x) + x];
+		minor_diag_sum += a[(size * (x + 1)) - (x + 1)];
 	}
 
 	printf("%d, %d\n", major_diag_sum, minor_diag_sum);
