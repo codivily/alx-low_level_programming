fb0e0e280349c3b365cfb963c2671c2651262327
codivily <codivily@gmail.com>
2022-03-29 18:53:30 -0700

    0-memset.c fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,8 @@
-
 /**
  * _memset - fills memory with a constant byte
  * @s: memory area pointed
- * @b: constant byte
- * @n: bytes of the memory area 
+ * @b: constant b
+ * @n: first n bytes count
  *
  * Return: A pointer to the memory area s
  */
@@ -12,10 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i = 0;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
 
 	return (s);
 }
-
