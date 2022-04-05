7773561945f01c6327c54e77b936fff6e72b487f
codivily <codivily@gmail.com>
2022-04-05 14:45:31 -0700

    0-create_array.c fix mixed declaration

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,9 +9,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array = NULL;
+
 	if (size <= 0)
 		return (NULL);
-	char *array = (char *)malloc(sizeof(char) * size);
+
+	array = (char *)malloc(sizeof(char) * size);
 
 	if (array != NULL)
 	{
