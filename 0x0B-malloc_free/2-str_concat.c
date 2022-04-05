a8afff1f44ceb03df7bfefef4e348a0b79c79736
codivily <codivily@gmail.com>
2022-04-05 15:08:54 -0700

    2-str_concat.c fix 5

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,18 +14,18 @@ char *str_concat(char *s1, char *s2)
 	unsigned int s_len = 0;
 	unsigned int i = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
 	/* compute s1 & s2 lenghts */
-	while (*(s1 + i) != '\0')
+	while (s1 && *(s1 + i) != '\0')
 		i++;
 	s_len = i;
 
 	i = 0;
-	while (*(s2 + i) != '\0')
+	while (s2 && *(s2 + i) != '\0')
 		i++;
 	s_len += i;
+
+	if (s_len == 0)
+		return (NULL);
 
 	/* allocating memory */
 	s = malloc(sizeof(char) * (s_len + 1));
@@ -35,10 +35,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		i = 0;
 
-		while (*s1 != '\0')
+		while (s1 && *s1 != '\0')
 			s[i++] = *(s1++);
 
-		while (*s2 != '\0')
+		while (s2 && *s2 != '\0')
 			s[i++] = *(s2++);
 
 		s[i] = '\0';
