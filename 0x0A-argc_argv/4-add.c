94f5197364bd90ceb3b11da06d34905364f7cb42
codivily <codivily@gmail.com>
2022-04-05 00:37:56 -0700

    4-add.c fix 2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
+/**
+ * itoa - convert an integer to a string
+ * @val: An integer
+ * @base: An base
+ *
+ * Return: the converted string
+ */
+char *itao(int val, int base)
+{
+	static char buffer[32] = {0};
+
+	int i = 30;
+
+	for (; val && i; --i, val /= base)
+		buffer[i] = "0123456789abcdef"[val % base];
+
+	return (buffer + i + 1);
+}
 /**
  * main - A program that adds positive numbers
  * @argc: arugment count
@@ -14,16 +33,30 @@ int main(int argc, char **argv)
 
 	int sum = 0;
 	int num = 0;
+	char *str = NULL;
 
 	while (argc >= 1)
 	{
-		num = atoi(argv[argc - 1]);
+		str = argv[argc - 1];
+		num = atoi(str);
 
-		if (num > 0)
-			sum += atoi(argv[argc - 1]);
+		if (num >= 0 && strlen(str) == strlen(itao(num, 10)))
+			sum += num;
+		else
+			break;
 
 		argc--;
 	}
 
+
+	if (argc == 1)
+		printf("%d\n", sum);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	return (0);
+
 }
