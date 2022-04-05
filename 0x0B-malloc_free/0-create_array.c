2b6f8e5424af5afd78257f0eed752294990c278c
codivily <codivily@gmail.com>
2022-04-05 14:43:52 -0700

    0-create_array.c fix 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	if (size <= 0)
 		return (NULL);
-	char *array = malloc(sizeof(char) * size);
+	char *array = (char *)malloc(sizeof(char) * size);
 
 	if (array != NULL)
 	{
