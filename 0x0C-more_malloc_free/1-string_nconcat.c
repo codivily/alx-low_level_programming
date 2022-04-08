6e0704e3182aa9eb31a3087fc507f51e819c67dc
codivily <codivily@gmail.com>
2022-04-08 02:47:54 -0700

    1-string_nconcat.c fix 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,6 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n > s_len)
 		n = s_len;
+	else if (n < 0)
+	{
+		n = 0;
+		s_len = 0;
+	}
 
 	i = 0;
 	while (s1 != NULL && s1[i] != '\0')
