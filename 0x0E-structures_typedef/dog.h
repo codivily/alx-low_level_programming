06bed3119b801b5117f45258c709b9f4cbac5d52
codivily <codivily@gmail.com>
2022-04-12 17:02:41 -0700

    dog.h added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - A struct that represent a dog
+ * @name: the dog name
+ * @age: the age
+ * @owner: the owner name
+ *
+ * Description: A go structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H_ */
