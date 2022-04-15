d21f904cdbbca29bf1ce0dcb6b8fd5f868330ab8
codivily <codivily@gmail.com>
2022-04-15 02:14:40 -0700

    3-main.c updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,6 @@
 int main(int argc, char **argv)
 {
 	int a = 0, b = 0, i = 0;
-
 	int (*op)(int, int) = NULL;
 
 	if (argc != 4)
@@ -31,11 +30,6 @@ int main(int argc, char **argv)
 		i++;
 	b = (int)argv[3][i];
 
-	if (a != '\0' || b != '\0')
-	{
-		printf("Error\n");
-		return (-1);
-	}
 
 	a = atoi(argv[1]);
 	op = get_op_func(argv[2]);
