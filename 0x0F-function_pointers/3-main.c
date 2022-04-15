e84c90b59c7dfd9c3e9f18549b1684ccae222f8c
codivily <codivily@gmail.com>
2022-04-15 02:34:28 -0700

    3-main.c updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char **argv)
 {
-	int a = 0, b = 0;
+	int a = 0, b = 0, i = 0;
 	int (*op)(int, int) = NULL;
 
 	if (argc < 4)
@@ -20,6 +20,22 @@ int main(int argc, char **argv)
 		return (-1);
 	}
 
+
+	while ('0' <= argv[1][i] && argv[1][i] <= '9')
+		i++;
+	a = (int)argv[1][i];
+
+	i = 0;
+	while ('0' <= argv[3][i] && argv[3][i] <= '9')
+		i++;
+	b = (int)argv[3][i];
+
+	if (a != (int)'\0' || b != (int)'\0')
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	
 	a = atoi(argv[1]);
 	op = get_op_func(argv[2]);
 	b = atoi(argv[3]);
