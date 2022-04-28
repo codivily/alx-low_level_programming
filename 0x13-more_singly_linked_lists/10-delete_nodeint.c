e7eb44048befcc1c844e2b9f68ca3b0da889843e
codivily <codivily@gmail.com>
2022-04-27 19:41:44 -0700

    10-delete_nodeint.c fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (tmp != NULL && index-- > 1)
+	while (tmp != NULL && index-- > 0)
 		tmp = tmp->next;
 
 	if (tmp == NULL || tmp->next == NULL)
